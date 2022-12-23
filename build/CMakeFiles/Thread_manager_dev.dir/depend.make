9c537375585763be7a5c3509e76eb6d9d9e148fe
Adam <wasyl6610@gmail.com>
2022-12-23 13:19:44 +0100

    new ignore

--- build/CMakeFiles/Thread_manager_dev.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Thread_manager_dev.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adam/Projects/Threads/Threads_Manager
-# Build directory: /home/adam/Projects/Threads/Threads_Manager/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
